b5e76280317271aea6b652c79b7403d105543313
AsishAB <asish.bishoi@gmail.com>
2022-04-21 20:06:41 +0530

    Integrated Express ,and created a server

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ const express = require("express");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
+app.use('/go',  (req, res, next) => {
+    res.send("<h1> Hello From Go </h1>")
+});
+app.use('/',  (req, res, next) => {
+    res.send("<h1> Hello From Node </h1>");
+   
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port} - http://localhost:${port}`)
+});
