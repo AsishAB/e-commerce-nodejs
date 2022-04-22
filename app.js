66a4cf7e526e43662c1f468f1e37e49edebc1c26
AsishAB <asish.bishoi@gmail.com>
2022-04-22 21:51:37 +0530

    Added Router

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 
 const express = require("express");
 
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 const port = process.env.PORT || 3000;
 
 app.use('/go',  (req, res, next) => {
     res.send("<h1> Hello From Go </h1>")
 });
+
+app.use('/product', (req, res, next) => {
+    res.redirect('/go');
+});
 app.use('/',  (req, res, next) => {
     res.send("<h1> Hello From Node </h1>");
    
 });
+
+
+
 
 
 app.listen(port, () => {
