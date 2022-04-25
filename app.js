b2ef2eedda3f8675a365a397fb6efdd29de257a5
AsishAB <asish.bishoi@gmail.com>
2022-04-25 17:53:40 +0530

    Modified app.js file

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({extended:false}));
 
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
-
+app.use((req, res, next) => {
+    res.status(404).send("<h1>404 Error </h1><p style='color:red;'>Page Not Found</p>")
+})
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
