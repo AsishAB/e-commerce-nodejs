471ccbb1f0068d7d5f564fe03145930ff8ec8b23
AsishAB <asish.bishoi@gmail.com>
2022-04-29 21:42:45 +0530

    Added 404 controller

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const rootDir = require("./helpers/user-defined-path");
+const htmlError = require('./controllers/HtmlErrorController');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
@@ -33,11 +34,8 @@ app.use('/shop' ,shopRoutes);
 
 
 app.use(express.static(path.join(__dirname, 'public'))); //express.static is used to serve static files like css or js files
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404-error.html'));
-    //res.status(404).sendFile(path.join(rootDir, 'views', '/htmlerrors/404-error.html'));
-    res.status(404).render('404-error.ejs', {pageTitle: "Page Not Found"});
-});
+
+app.use(htmlError.get404Page); //To display 404 page
 
 const port = process.env.PORT || 3000;
 
