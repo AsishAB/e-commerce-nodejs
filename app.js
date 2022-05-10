eb69a4621788a88d4c04c52eb3467e318c4c38f1
AsishAB <asish.bishoi@gmail.com>
2022-05-10 18:32:01 +0530

    Added Sequelize, and users table

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const shopRoutes = require("./routes/shop");
 const rootDir = require("./helpers/user-defined-path");
 const htmlError = require('./controllers/HtmlErrorController');
 const db = require('./helpers/database-mysql');
+const sequelize = require('./helpers/database-using-sequelize');
+
+
+
 
 db.query("SELECT * FROM tbl_products").then(result => {
     // console.log(result[0]);
@@ -45,6 +49,35 @@ app.use('/shop' ,shopRoutes);
 app.use(express.static(path.join(__dirname, 'public'))); //express.static is used to serve static files like css or js files
 
 app.use(htmlError.get404Page); //To display 404 page
+
+app.use( (req, res, next) => {
+    db.query("SELECT * FROM tbl_users WHERE TUM_Id = ? ", [1])
+        .then( ([rows]) => {
+            console.log("In app.js");
+            console.log(rows[0].TUM_Id);
+            req.users = rows[0].TUM_Id;
+        })
+        .catch(err => {
+            console.log("In app.js");
+            console.log(err); 
+        });
+
+        next();
+        
+});
+
+
+/*  sequelize.sync() is used to generate tables for all Sequelise objects (example- cart, product etc.) directly in the database */
+sequelize.sync()
+    .then(result => {
+        //console.log(result);
+    })
+    .catch(err => {
+        // console.log("Inside app.js");
+        // console.log(err);
+    });
+
+  
 
 const port = process.env.PORT || 3000;
 
