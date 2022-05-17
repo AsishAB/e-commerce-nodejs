0967c790547b4f4390837b163bfa0bde3f9b1aaf
Asish <58765530+AsishAB@users.noreply.github.com>
2022-05-17 12:27:51 +0530

    Delete mongodb-url.js

--- helpers/passwords/mongodb-url.js
+++
@@ -1,5 +0,0 @@
-
-const mongoDBPassword = "vUarAawV333Dx2n1";
-const mongoDBURL = "mongodb+srv://RememberMeAsish:"+mongoDBPassword+"@asishcluster.rjwov.mongodb.net/?retryWrites=true&w=majority";
-
-module.exports = mongoDBURL;
