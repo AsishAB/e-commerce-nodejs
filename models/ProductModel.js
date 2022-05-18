029315a618924e5bb86c3edbc2d8b323c6c114ae
AsishAB <asish.bishoi@gmail.com>
2022-05-18 08:28:56 +0530

    Made changes to ProductModel.

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -3,12 +3,12 @@ const getDB = require('../helpers/database-mongodb').getDB;
 
 module.exports = class Product {
     constructor(id,tlt, desc='', imgURL, price) {
-        this.productId = id;
+        this.TP_ProductId = id;
         
-        this.title = tlt;
-        this.description = desc;
-        this.imageURL = (imgURL != '') ? imgURL : "https://live.staticflickr.com/5217/5471047557_4dc13f5376_n.jpg";
-        this.price = price;
+        this.TP_Product_Title = tlt;
+        this.TP_Product_Description = desc;
+        this.TP_Image_URL = (imgURL != '') ? imgURL : "https://live.staticflickr.com/5217/5471047557_4dc13f5376_n.jpg";
+        this.TP_Product_Price = price;
     }
 
     save() {
